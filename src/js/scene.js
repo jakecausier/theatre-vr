e6fd5304ede3fb8b6af799f2e2087e6022e15f26
Jake Causier <jake@purplemountmedia.com>
2019-12-26 01:41:59 +0000

    Scaled scene smaller to ensure VR camera position is correct

--- src/js/scene.js
+++ src/js/scene.js
@@ -2,51 +2,46 @@
 const THREE = require('three')
 
 import videojs from 'video.js'
-import FastAverageColor from 'fast-average-color';
+import FastAverageColor from 'fast-average-color'
 import { VRButton } from 'three/examples/jsm/webxr/VRButton.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { CSS3DRenderer, CSS3DObject } from 'three/examples/jsm/renderers/CSS3DRenderer.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { CSS3DRenderer, CSS3DObject } from 'three/examples/jsm/renderers/CSS3DRenderer.js'
 
-var loader = new GLTFLoader();
-var renderer, controls, camera, scene, targetMesh, targetBox, middleColour, lightScreen;
-var video, player, videoImage, videoImageContext, videoTexture;
-var fastColour = new FastAverageColor();
+var loader = new GLTFLoader()
+var container
+var renderer, controls, camera, scene, targetMesh, targetBox, middleColour, lightScreen
+var video, player, videoImage, videoImageContext, videoTexture
+var fastColour = new FastAverageColor()
+
+var enableControls = false
 
 videojs.log.level('off')
 
-init();
-animate();
 
+var startButton = document.getElementById('startButton')
+startButton.addEventListener('click', function () {
+  init()
+  animate()
+}, false )
 
-function load() {
-  loader.load('../../models/TheatreEmpty/TheatreEmpty.gltf',
-    function (gltf) {
-      console.log('Object loaded!')
-      scene.add(gltf.scene)
-    },
-    function (xhr) {
-      console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-    },
-    function ( error ) {
-      console.log('An error happened while loading the model');
-    }
-  );
-}
 
 function init() {
 
-  scene = new THREE.Scene();
-  window.scene = scene;
+  var overlay = document.getElementById('overlay')
+  overlay.remove()
 
-  load();
+  scene = new THREE.Scene()
+  window.scene = scene
 
-  video = document.getElementById('player');
-  player = videojs('player');
-  player.play();
+  load()
 
-  var screenWidth = 6500
-  var screenHeight = 3375
+  video = document.getElementById('player')
+  player = videojs('player')
+  player.play()
+
+  var screenWidth = (1280 / 2) // 6500
+  var screenHeight = (720 / 2) // 3375
 
   var invisiMat = new THREE.MeshBasicMaterial();
   invisiMat.transparent = true;
@@ -63,31 +58,32 @@ function init() {
   videoTexture.magFilter = THREE.LinearFilter;
 
 
-  var screenMat = new THREE.MeshBasicMaterial({ map: videoTexture });
+  var screenMat = new THREE.MeshBasicMaterial({ map: videoTexture })
+  screenMat.encoding = THREE.sRGBEncoding
   var screenGeo = new THREE.PlaneBufferGeometry(screenWidth, screenHeight, 1, 1)
-  var screen = new THREE.Mesh(screenGeo, screenMat);
-  screen.position.set(0, 2400, 7800);
-  screen.lookAt(0, 2400, 0);
-  scene.add(screen);
+  var screen = new THREE.Mesh(screenGeo, screenMat)
+  screen.position.set(0, 240, 780)
+  screen.lookAt(0, 240, 0)
+  scene.add(screen)
 
 
   targetBox = new THREE.Object3D();
-  targetBox.position.set(0, 0, 8000)
+  targetBox.position.set(0, 0, 800)
   scene.add(targetBox);
 
   lightScreen = new THREE.SpotLight(0x000000, 0.05)
-  lightScreen.position.set(0, 8000, 8000)
+  lightScreen.position.set(0, 800, 800)
   lightScreen.target = targetBox
   lightScreen.angle = 1.3
   lightScreen.penumbra = 0.2
   lightScreen.decay = 2
-  lightScreen.distance = 15000
+  lightScreen.distance = 1500
   scene.add(lightScreen)
 
 
-  setupRoofLight(6000, 10000, 0xFFDD80, 0.5)
-  setupRoofLight(-2000, 10000, 0xFFDD80, 0.5)
-  setupRoofLight(-8000, 10000, 0xFFDD80, 0.5)
+  setupRoofLight(600, 1000, 0xFFDD80, 0.5)
+  setupRoofLight(-200, 1000, 0xFFDD80, 0.5)
+  setupRoofLight(-800, 1000, 0xFFDD80, 0.5)
 
 
   renderer = new THREE.WebGLRenderer({ antialias: true });
@@ -99,14 +95,20 @@ function init() {
   document.body.appendChild(VRButton.createButton(renderer));
 
 
-  camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 40000);
-  camera.position.set( 0, 1600, 2000 );
+  camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 4000);
+  camera.position.set( 0, 160, 200 );
   camera.lookAt(screen.position);
   camera.layers.enable( 1 )
 
 
-  // controls = new OrbitControls( camera, renderer.domElement );
-  // controls.update();
+  if ( enableControls ) {
+    controls = new OrbitControls( camera, renderer.domElement );
+    controls.enableDamping = true
+    controls.dampingFactor = 0.05
+    controls.minDistance = 100
+    controls.maxDistance = 600
+    controls.update();
+  }
 
 
   // Listen for if the window is resized
@@ -123,9 +125,29 @@ function init() {
     lightScreen.angle = 1.3
     lightRoof.penumbra = 1
     lightRoof.decay = 1
-    lightRoof.distance = 20000
+    lightRoof.distance = 2000
     scene.add(lightRoof)
   } 
+
+}
+
+function load() {
+  var obj
+
+  loader.load('../../models/TheatreEmpty/TheatreEmpty.gltf',
+    function (gltf) {
+      obj = gltf.scene
+      obj.scale.set(0.1, 0.1, 0.1)
+      scene.add(obj)
+    },
+    function (xhr) {
+      console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+    },
+    function (error) {
+      console.log('An error happened while loading the model');
+    }
+  )
+
 
 }
 
@@ -138,13 +160,15 @@ function render() {
   videoImageContext.drawImage(video, 0, 0)
   renderer.render(scene, camera)
 
-  // controls.update()
-
   middleColour = fastColour.getColor(videoImageContext.canvas)
   lightScreen.color = new THREE.Color(middleColour.value[0], middleColour.value[1], middleColour.value[2], 1)
 
   if (videoTexture) {
     videoTexture.needsUpdate = true
+  }
+
+  if ( enableControls ) {
+    controls.update()
   }
 }
 
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,4 @@
 const mix = require('laravel-mix');
 
-mix.js('src/js/scene.js', 'build/js')
-   .js('src/js/video.js', 'build/js')
-   .sass('src/scss/style.scss', 'build/css')
-   .version();
+mix.js('src/js/scene.js', 'build/js/scene.js')
+   .sass('src/scss/style.scss', 'build/css/style.css').options({ processCssUrls: false });
