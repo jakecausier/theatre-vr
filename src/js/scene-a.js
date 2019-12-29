ba8ce04d185e348178884db385cc9c51a9d013b9
Jake Causier <jake@purplemountmedia.com>
2019-12-29 00:33:00 +0000

    Changed UI elements for the time being and added volume slider

--- src/js/scene-a.js
+++ src/js/scene-a.js
@@ -1,6 +1,12 @@
 
 require('aframe')
+require('aframe-gui')
 require('aframe-event-set-component')
+
+
+// To-Add
+// https://www.npmjs.com/package/aframe-particle-system-component
+
 
 window.THREE.TextureLoader.prototype.crossOrigin = '';
 window.THREE.ImageLoader.prototype.crossOrigin = '';
@@ -9,17 +15,25 @@ window.THREE.ImageUtils.crossOrigin = '';
 window.THREE.gammaOutput = true;
 window.THREE.gammaFactor = 2.2;
 
-AFRAME.registerComponent('ui-playbutton', {
-  init: function () {
-    var el = this.el;
-    el.addEventListener('mouseenter', function () {
-      document.player.play()
-    });
-  }
-});
 
-// AFRAME.registerComponent('ui-fade-hide', function() {
+var playerSlider, defaultVolume, newVolume;
 
-// });
+
+window.startVideo = function () {
+  playerSlider = document.querySelector('#player-volume');
+  defaultVolume = playerSlider.getAttribute('percent');
+  console.log('Getting default volume from element...', defaultVolume)
+  document.player.volume(defaultVolume);
+  document.player.play();
+}
+
+window.changeVolume = function (event, data) {
+  newVolume = data.toFixed(2);
+  console.log('Setting new volume...', newVolume)
+  playerSlider = document.querySelector('#player-volume');
+  playerSlider.setAttribute('percent', newVolume);
+  document.player.volume(newVolume);
+}
+
 
 // THREE.WebGLState: DOMException: Failed to execute 'texImage2D' on 'WebGLRenderingContext': The video element contains cross-origin data, and may not be loaded.
